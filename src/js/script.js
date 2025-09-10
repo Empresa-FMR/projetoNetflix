c965d44c8a9b792b9cff2683951313f7490e0e63
Felipe <felipe.mreinoso@outlook.com>
2025-09-10 10:50:05 -0300

    8 - Adicionando o Javascript do projeto

--- src/js/script.js
+++ src/js/script.js
@@ -1,22 +1,53 @@
-// DECLARANDO AS VARIAVEIS COM DOM (DOCUMENT MODEL OBJECT)
-const carrosselLista = document.querySelector(".carrossel-lista");
-const carrosselItem = document.querySelectorAll(".carrossel-item");
-const botaoAnterior =document.getElementsByClassName("btn-anterior");
-const botaoProximo =document.getElementsByClassName("btn-proximo");
+//ARRAY DE OBJETOS COM AS IMAGENS E INFORMAÇÕES
 
-let contador=0;
-const larguraItem=carrosselItem[0].offsetWitdh +10;// largura do item + margem
+const bannerItems =[
+{
+    imagem:"../assets/Game1.jpg",
+    titulo: "titulo do filme 1",
+    descricao: "Filme 1"
+},
 
-botaoProximo.addEventListener("click",()=>{
-    if(contador <carrosselItem.length -1){
-        contador++;
-        carrosselLista.style.transform =`translateX(-${contador * larguraItem}px)`;
+{
+    imagem:"../assets/Game2.jpg",
+    titulo: "titulo do filme 2",
+    descricao: "Filme 2"
+},
+
+{
+    imagem:"../assets/Game3.jpg",
+    titulo: "titulo do filme 3",
+    descricao: "Filme 3"
+},
+];
+
+// DECLARANDO AS VARIAVEIS E ELEMENTOS COM DOM 
+const tempo = 5000; // tempo em milisegundos
+const elementoBanner = document.querySelector (".banner");
+const elementoTitulo = document.querySelector (".banner-titulo");
+const elementoDescricao = document.querySelector (".banner-descricao");
+let i=0; // inicio da contagem das imagens
+
+// FUNÇÃO PARA O SLIDESSHOW
+
+function slideShow(){
+    // altera a imagem de fundo do banner
+    // `` - Template Strings ou Strings Literais
+    elementoBanner.style.backgroundImage = `url(${bannerItems}[i].imagem)`
+    // ALTERA O TÍTULO DO BANNER
+    elementoTitulo.textContent = bannerItems[i].titulo;
+    elementoDescricao.textContent = bannerItems[i].descricao;
+
+    // INCREMENTA O ÍNDICE (I) E REINICIA QUANDO CHEGAR NO FINAL DO ARRAY
+
+    // Se o I for maior que a quantidade de imagens volta para o início
+
+    i++;
+    if ( i > bannerItems.lenght){
+        i = 0; 
     }
-})
+    // chama a função novamente após um tempo definido
+    setTimeout(slideShow, tempo);
+}
 
-botaoAnterior.addEventListener("click",()=>{
-    if(contador > 0){
-        contador--;
-        carrosselLista.style.transform =`translateX(-${contador * larguraItem}px)`;
-    }
-})
+//inicia o slideshow da função
+slideshow();

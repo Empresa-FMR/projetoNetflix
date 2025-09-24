f6a777b770b4e1d04c109c31b203bfc1b617615c
Felipe <felipe.mreinoso@outlook.com>
2025-09-24 10:45:41 -0300

    12 - Criando a página de Conteúdo

--- src/js/script.js
+++ src/js/script.js
@@ -1,18 +1,18 @@
 //ARRAY DE OBJETOS COM AS IMAGENS E INFORMAÇÕES DO BANNER
 const bannerItems =[
 {
-    imagem:"./src/assets/Game1.jpg",
+    imagem:"./src/assets/img/Game1.jpg",
     titulo: "TItulo do filme 1",
     descricao: "Filme 1"
 },
 {
-    imagem:"./src/assets/Game2.jpg",
+    imagem:"./src/assets/img/Game2.jpg",
     titulo: "TItulo do filme 2",
     descricao: "Filme 2"
 },
 
 {
-    imagem:"./src/assets/Game3.jpg",
+    imagem:"./src/assets/img/Game3.jpg",
     titulo: "TItulo do filme 3",
     descricao: "Filme 3"
 }
@@ -30,7 +30,7 @@ const elementoDescricao = document.querySelector(".banner-descricao");
 // FUNÇÃO PARA O SLIDESHOW
 
 function slideShow(){
-    // ALTERA A IMAEM DE FUNDO DO BANNER
+    // ALTERA A IMAGEM DE FUNDO DO BANNER
     // ``- template strings ou string literais forma de concatenar
     elementoBanner.style.backgroundImage = `url(${bannerItems[i].imagem})`;
     // ALTERA O TITULO DO BANNER
